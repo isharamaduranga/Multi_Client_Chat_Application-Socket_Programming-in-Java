dc482722c63c72dfff97d43f5d843d62ebbc3402
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-08-09 09:58:54 +0530

    Introduced Server side with Server & ClientHandler Classes.

---
+++ Server_Side/src/Server.java
@@ -0,0 +1,10 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Multi_Client_Chat_Application
+ * Date        : 8/9/2022
+ * Time        : 9:55 AM
+ * Year        : 2022
+ */
+
+public class Server {
+}
