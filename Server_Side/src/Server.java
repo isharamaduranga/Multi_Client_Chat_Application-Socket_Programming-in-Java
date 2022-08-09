f53c1a2523715426f77e5b4cc2dfd7ea3f4a4ccc
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-08-09 14:00:41 +0530

    Complted Server side Programming..

---
+++ Server_Side/src/Server.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Multi_Client_Chat_Application
+ * Date        : 8/9/2022
+ * Time        : 1:51 PM
+ * Year        : 2022
+ */
+
+public class Server {
+    /** Create static ArrayList with handle for clients*/
+    private static ArrayList<ClientHandler> clients = new ArrayList<ClientHandler>();
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket;
+        Socket socket;
+        try {
+            serverSocket = new ServerSocket(5006);
+            while(true) {
+                System.out.println("Waiting for clients...");
+                socket = serverSocket.accept();
+                System.out.println("Connected Successfully...");
+
+                /** passed socket and client Array to ClientHandler Class and Start Thread...*/
+                ClientHandler clientThread = new ClientHandler(socket, clients);
+                clients.add(clientThread);
+                clientThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
