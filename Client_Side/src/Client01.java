da89121cf5f4551cfdc21107a24aaab58c9213f9
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-08-09 10:50:57 +0530

    Added Client Appinitializer..

---
+++ Client_Side/src/Client01.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Client01 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("View/Login_Signup.fxml"));
+        primaryStage.setTitle("Group-Messenger!");
+        primaryStage.setScene(new Scene(root, 330, 560));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
