8850ffba89337212ef617d5694dca72d84c2db7d
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-08-09 11:34:44 +0530

    Added method of change opacity in labels and makeDefault method.

--- Client_Side/src/Controller/Login_Signup.java
+++ Client_Side/src/Controller/Login_Signup.java
@@ -58,10 +58,50 @@ public class Login_Signup {
                     newUser.email = regEmail.getText();
                     newUser.fullName = regFirstName.getText();
                     newUser.phoneNo = regPhoneNo.getText();
-
+                    if (male.isSelected()) {
+                        newUser.gender = "Male";
+                    } else {
+                        newUser.gender = "Female";
+                    }
+                    users.add(newUser);
+                    goBack.setOpacity(1);
+                    success.setOpacity(1);
+                    makeDefault();
+                    if (controlRegLabel.getOpacity() == 1) {
+                        controlRegLabel.setOpacity(0);
+                    }
+                    if (nameExists.getOpacity() == 1) {
+                        nameExists.setOpacity(0);
+                    }
+                } else {
+                    checkEmail.setOpacity(1);
+                    setOpacity(nameExists, goBack, controlRegLabel, success);
                 }
+            } else {
+                nameExists.setOpacity(1);
+                setOpacity(success, goBack, controlRegLabel, checkEmail);
             }
+        } else {
+            controlRegLabel.setOpacity(1);
+            setOpacity(success, goBack, nameExists, checkEmail);
+
         }
+    }
+
+    private void setOpacity(Label a, Label b, Label c, Label d) {
+        if(a.getOpacity() == 1 || b.getOpacity() == 1 || c.getOpacity() == 1 || d.getOpacity() == 1) {
+            a.setOpacity(0);
+            b.setOpacity(0);
+            c.setOpacity(0);
+            d.setOpacity(0);
+        }
+    }
+
+
+    private void setOpacity(Label controlRegLabel, Label checkEmail, Label nameExists) {
+        controlRegLabel.setOpacity(0);
+        checkEmail.setOpacity(0);
+        nameExists.setOpacity(0);
     }
 
     private boolean checkUser(String username) {
@@ -80,6 +120,16 @@ public class Login_Signup {
             }
         }
         return true;
+    }
+
+    private void makeDefault() {
+        regName.setText("");
+        regPass.setText("");
+        regEmail.setText("");
+        regFirstName.setText("");
+        regPhoneNo.setText("");
+        male.setSelected(true);
+        setOpacity(controlRegLabel, checkEmail, nameExists);
     }
 
 
